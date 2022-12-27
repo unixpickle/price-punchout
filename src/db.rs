82fd3af330777558e4cea5301a63c62a7ec0fe03
Alex Nichol <unixpickle@gmail.com>
2022-12-26 21:34:24 -0500

    database schema and insertions

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,8 @@
+use std::fmt::Write;
 use std::path::Path;
 use std::sync::Arc;
 
-use rusqlite::Connection;
+use rusqlite::{Connection, Transaction};
 use sha2::Digest;
 use tokio::{sync::Mutex, task::spawn_blocking};
 
@@ -27,16 +28,165 @@ impl Database {
             db: Arc::new(Mutex::new(conn)),
         })
     }
+
+    // Either insert a new listing, or update the information if the website_id
+    // is already present in the database.
+    pub async fn insert_or_update(&self, listing: Listing) -> anyhow::Result<()> {
+        self.with_db(move |db| {
+            let mut tx = db.transaction()?;
+            let blob_id = insert_blob(&mut tx, &listing.image_data)?;
+            let result: rusqlite::Result<(i64, i64)> = tx.query_row(
+                "SELECT id, image_blob FROM listings WHERE website=?1, website_id=?2",
+                (&listing.website, &listing.website_id),
+                |row| Ok((row.get(0)?, row.get(1)?)),
+            );
+            match result {
+                Ok((id, old_image_blob)) => {
+                    tx.execute(
+                        "
+                            UPDATE listings
+                            SET image_blob = ?1,
+                                price = ?2,
+                                title = ?3,
+                                last_seen = unixepoch()
+                            WHERE id=?4
+                        ",
+                        rusqlite::params![blob_id, listing.price, listing.title, id],
+                    )?;
+                    garbage_collect_blob(&mut tx, old_image_blob)?;
+                }
+                Err(rusqlite::Error::QueryReturnedNoRows) => {
+                    tx.execute(
+                        "
+                        INSERT INTO listings (
+                            created,
+                            last_seen,
+                            website,
+                            website_id,
+                            price,
+                            title,
+                            image_blob
+                        ) VALUES (
+                            unixepoch(),
+                            unixepoch(),
+                            ?1,
+                            ?2,
+                            ?3,
+                            ?4,
+                            ?5
+                        )
+                    ",
+                        rusqlite::params![
+                            listing.website,
+                            listing.website_id,
+                            listing.price,
+                            listing.title,
+                            blob_id
+                        ],
+                    )?;
+                }
+                x @ Err(_) => {
+                    x?;
+                }
+            }
+            Ok(())
+        })
+        .await
+    }
+
+    async fn with_db<
+        T: 'static + Send,
+        F: 'static + Send + FnOnce(&mut Connection) -> anyhow::Result<T>,
+    >(
+        &self,
+        f: F,
+    ) -> anyhow::Result<T> {
+        let db_ref = self.db.clone();
+        spawn_blocking(move || {
+            let mut db = db_ref.blocking_lock();
+            f(&mut db)
+        })
+        .await?
+    }
 }
 
 fn create_tables(conn: &Connection) -> anyhow::Result<()> {
     conn.execute(
-        "CREATE TABLE if not exists thumbnails (
+        "CREATE TABLE if not exists listings (
             id           INTEGER PRIMARY KEY,
+            created      INTEGER,
+            last_seen     INTEGER,
             website      CHAR(32),
-            website_id   CHAR(32)
+            website_id   CHAR(32),
+            price        INTEGER,
+            title        CHAR(128),
+            image_blob   INTEGER,
+            UNIQUE (website, website_id)
         )",
         (),
     )?;
+    conn.execute(
+        "CREATE TABLE if not exists blobs (
+            id           INTEGER PRIMARY KEY,
+            hash         CHAR(32),
+            data         BLOB,
+            UNIQUE hash
+        )",
+        (),
+    )?;
+    conn.execute(
+        "CREATE INDEX listings_website_id ON listings(website, website_id)",
+        (),
+    )?;
+    conn.execute(
+        "CREATE INDEX listings_image_blob ON listings(image_blob)",
+        (),
+    )?;
+    conn.execute("CREATE INDEX images_hash ON images(hash)", ())?;
     Ok(())
+}
+
+fn insert_blob(tx: &mut Transaction, blob: &[u8]) -> rusqlite::Result<i64> {
+    let hash = hash_blob(blob);
+    let result = tx.execute(
+        "INSERT OR IGNORE INTO blobs (hash, data) VALUES (?1, ?2)",
+        rusqlite::params![&hash, blob],
+    )?;
+    if result == 1 {
+        Ok(tx.last_insert_rowid())
+    } else {
+        tx.query_row("SELECT id FROM blobs WHERE hash=?1", (hash,), |row| {
+            row.get::<_, i64>(0)
+        })
+    }
+}
+
+fn garbage_collect_blob(tx: &mut Transaction, id: i64) -> rusqlite::Result<()> {
+    let count: i64 = tx.query_row(
+        "SELECT COUNT(*) FROM listings WHERE image_blob=?1",
+        (id,),
+        |row| row.get(0),
+    )?;
+    if count == 0 {
+        tx.execute("DELETE FROM blobs WHERE id=?1", (id,))?;
+    }
+    Ok(())
+}
+
+fn hash_blob(data: &[u8]) -> String {
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(data);
+    let mut res = String::with_capacity(32);
+    for ch in &hasher.finalize()[0..16] {
+        write!(&mut res, "{:02x}", ch).unwrap();
+    }
+    res
+}
+
+pub struct Listing {
+    website: String,
+    website_id: String,
+    price: i64,
+    title: String,
+    image_data: Vec<u8>,
 }
