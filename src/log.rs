219a84ccc2c10443696b2f2ec6c2d486de0f9085
Alex Nichol <unixpickle@gmail.com>
2022-12-31 00:40:06 -0500

    log helpers

---
+++ src/log.rs
@@ -0,0 +1,9 @@
+#[macro_export]
+macro_rules! log_async {
+    ($db:expr, $formatstr:expr, $($args:tt),*) => {{
+        let source = format!("{}:{}", file!(), line!());
+        let message = format!($formatstr, $($args),*);
+        eprintln!("{}: {}", source, message);
+        $db.insert_log_message(source, message).await?;
+    }};
+}
