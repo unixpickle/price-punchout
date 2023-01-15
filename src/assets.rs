45306e3ea8d48f008d01e7d32c6e11d939fb776d
Alex Nichol <unixpickle@gmail.com>
2023-01-14 16:11:42 -0800

    add missing file

--- src/assets.rs
+++ src/assets.rs
@@ -17,6 +17,7 @@ const ASSETS: &'static [(&'static str, &'static [u8])] = &[
     asset_pair!("not_found.html"),
     asset_pair!("style.css"),
     asset_pair!("favicon.ico"),
+    asset_pair!("js/api.js"),
     asset_pair!("js/script.js"),
     asset_pair!("js/deps/babel-standalone@6.26.0.js"),
     asset_pair!("js/deps/react-dom@18.2.0.js"),
